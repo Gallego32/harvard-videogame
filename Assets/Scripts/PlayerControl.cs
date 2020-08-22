c9275cc2837c88a3622280a025e263c823915d96
Álvaro Gallego Martínez <alvarok047@gmail.com>
2020-08-22 13:15:30 +0200

    Fixed Player1 Behaviour ¬ Fixed animations behaviour and walking up slopes behaviour. Created Grounded Variable and changed between different physicsMaterial2D ¬

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -16,6 +16,9 @@ public class PlayerControl : MonoBehaviour
     // Manage RigidBody
     private Rigidbody2D rb;
 
+    // Manage CircleCollider
+    private CircleCollider2D cc;
+
     // Manage animations;
     private Animator animation;
 
@@ -27,12 +30,18 @@ public class PlayerControl : MonoBehaviour
     private bool jump = false;
     public float speed = 30f;
 
+    // Moving on slopes
+    public PhysicsMaterial2D friction;
+    public PhysicsMaterial2D noFriction;
+
 
     // Start is called before the first frame update
     void Start()
     {
         // Get Controller2D component
         controller = GetComponent<CharacterController2D>();
+        //Get CircleCollider2D component
+        cc = GetComponent<CircleCollider2D>();
         // Get Animator component
         animation = GetComponent<Animator>();
         // Get RigidBody2D Component
@@ -77,6 +86,17 @@ public class PlayerControl : MonoBehaviour
         controller.Move(xMovement * Time.fixedDeltaTime, false, jump);
         jump = false;
 
+        if (xMovement == 0) {
+            rb.sharedMaterial = friction;
+            cc.sharedMaterial = friction;
+        }
+        else {
+            rb.sharedMaterial = noFriction;
+            cc.sharedMaterial = noFriction;
+        }
+            
+        
+
         //Debug.Log("Sp = " + rb.velocity.y);
 
         // Behaviour for Animator
@@ -86,10 +106,17 @@ public class PlayerControl : MonoBehaviour
             if (rb.velocity.y > 1f)
                 animation.SetBool("Jump", true);
 
-            if (rb.velocity.y == 0f) 
-                animation.SetBool("Jump",false);
+            //if (rb.velocity.y == 0f && !animation.GetBool("Falling")) 
+                //animation.SetBool("Jump",false);
 
             animation.SetBool("Falling", false);
         }            
+
+        if (controller.m_Grounded) {
+            animation.SetBool("Jump",false);
+            animation.SetBool("Falling", false);
+            animation.SetBool("Grounded", true);
+        } else 
+            animation.SetBool("Grounded", false);
     }
 }
