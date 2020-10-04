45a995701d11c3f92db3bfa858b0b334508a9f32
Álvaro Gallego Martínez <alvarok047@gmail.com>
2020-10-05 01:59:30 +0200

    Improved Pause Menu UI

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -5,42 +5,40 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
+    // Control whether we are paused or not
     public static bool Paused = false;
 
+    // Set active if we are paused
     public GameObject pauseMenu;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
-        {   
             if (Paused)
-            {
                 Resume();
-                Paused = false;
-            } else
-            {
-                Pause();
-                Paused = true;
-            }
-                   
-        }
+            else
+                Pause();     
     }
     
     public void Resume()
     {
+        Paused = false;
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
     }
 
     void Pause()
     {
+        Paused = true;
         pauseMenu.SetActive(true);
         Time.timeScale = 0f;
     }
 
     public void Menu()
     {
+        Paused = false;
+        Time.timeScale = 1f;
         SceneManager.LoadScene("Menu");
     }
 
